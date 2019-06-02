4e2c2b549cb98771aad2c20341f4cdf37c8ee8bd
lucaslai10 <lucaslai10@yahoo.com.br>
2019-06-02 19:05:58 -0300

    detector de emocoes

---
+++ Keras/detector_emocao.py
@@ -0,0 +1,74 @@
+import numpy as np
+import argparse
+import cv2
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Flatten
+from keras.layers.convolutional import Conv2D
+from keras.optimizers import Adam
+from keras.layers.pooling import MaxPooling2D
+from keras.preprocessing.image import ImageDataGenerator
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def cria_model():
+    model = Sequential()
+
+    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
+    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(1024, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(7, activation='softmax'))
+    return model
+
+def get_emocao(nome_imagem):
+    model = cria_model()
+    model.load_weights('model.h5')
+
+    # prevents openCL usage and unnecessary logging messages
+    cv2.ocl.setUseOpenCL(False)
+
+    # dictionary which assigns each label an emotion (alphabetical order)
+    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+
+    facecasc = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    img = cv2.imread(nome_imagem)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = facecasc.detectMultiScale(img_gray,scaleFactor=1.3, minNeighbors=3)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+        roi_gray = img_gray[y:y + h, x:x + w]
+        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+        prediction = model.predict(cropped_img)
+        maxindex = int(np.argmax(prediction))
+        print(maxindex)
+        if maxindex == 0:
+            return emotion_dict[0] #Angry
+        elif maxindex == 3:
+            return emotion_dict[3] #Happy
+        elif maxindex == 5:
+            return emotion_dict[5] #Sad
+        else:
+            return emotion_dict[4] #Neutral
+
+emotion = get_emocao('eu.jpg')
+print(emotion)
+emotion = get_emocao('mulher_feliz.jpg')
+print(emotion)
